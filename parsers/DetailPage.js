486db11c6ef2795a35530c6c8589bdf07f0f092f
jasleen1291 <jasleenkaur1291@gmail.com>
2015-07-15 00:06:18 -0700

    final task movato plus store info

--- parsers/DetailPage.js
+++ parsers/DetailPage.js
@@ -1,18 +1,24 @@
 module.exports = function(fs, request, cheerio, task, out) {
-
-
-
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('52.2.127.199:27017/partb');
+var collection = db.get('zip');
+var city=task.city.replace("/ /g","-")
+var state=task.state
+var url="http://www.movoto.com/"+city+"-"+state+"/"+task.zip+"/demographics/"
+var url2="http://www.city-data.com/zips/"+task.zip+".html"
         //console.log(task.url);
-        request(task.url, function(error, response, html) {
+        request(url, function(error, response, html) {
                 if (!error) {
                     var $ = cheerio.load(html);
 
 
                     var json = {};
+                    json.zip=task.zip
                     var basicdata = ($('.basicData table tr td'));
 
                     basicdata.each(function(i, elem) {
-                        var key = ($(this).find("span").text())
+                        var key = ($(this).find("span").text().trim().replace("/ /g","_").split('.').join('').replace(":","").replace("$/","price_per"))
 
                         json[key] = $(this).text().replace(key, "");
                     });
@@ -21,7 +27,7 @@ module.exports = function(fs, request, cheerio, task, out) {
                     soldTitle.each(function(i, elem) {
                         var value = ($(this).next().text())
 
-                        json[$(this).text()] = value
+                        json[$(this).text().split("$").join("").split(".").join("")] = value
                     });
 
                     var tables = ($('.tableInfoValue table'));
@@ -29,7 +35,8 @@ module.exports = function(fs, request, cheerio, task, out) {
                         var key = ($(this).find("tr th"))
 
                         if (key.length == 1) {
-                            json[key.text()] = {}
+                        	keytext=key.text().split(".").join("")
+                            json[keytext] = {}
                             var rows = key.parent().nextAll()
                             rows.each(function(i, elem) {
                                 var tds = $(this).find("td")
@@ -37,13 +44,13 @@ module.exports = function(fs, request, cheerio, task, out) {
                                 tds.each(function(i, elem) {
 
                                     if (i === 0) {
-                                        currentKey = $(this).text().replace("\r\n", "").trim()
-                                        json[key.text()][currentKey] = {}
+                                        currentKey = $(this).text().replace("\r\n", "").trim().split("$").join("").split(".").join("")
+                                        json[keytext][currentKey] = {}
                                     } else if (i === 1) {
-                                        json[key.text()][currentKey]['number'] = $(this).text().replace("\r\n", "").trim()
+                                        json[keytext][currentKey]['number'] = $(this).text().replace("\r\n", "").trim()
 
                                     } else {
-                                        json[key.text()][currentKey]['percentage'] = $(this).text().replace("\r\n", "").replace("(", "").replace(")", "").replace("%", "").trim()
+                                        json[keytext][currentKey]['percentage'] = $(this).text().replace("\r\n", "").replace("(", "").replace(")", "").replace("%", "").trim()
 
                                     }
                                 })
@@ -61,21 +68,36 @@ module.exports = function(fs, request, cheerio, task, out) {
                     var tables = ($('.tableInfoValue').text().split(","));
                     //console.log(tables);
                     // json.val=tables;
+                    var Entities = require('html-entities').XmlEntities;
+ 
+entities = new Entities();
                    
-                    request("http://www.city-data.com/zips/99501.html", function(error, response, html) {
+                    request(url2, function(error, response, html) {
                             if (!error) {
                                 var $ = cheerio.load(html);
                                 var stores = {}
                                 $("#businesses tbody tr td:not([style])").each(function(i, elem) {
                                         if (i % 2 == 0) {
-                                            stores[$(this).text()] = Number($(this).next().text())
+                                        	
+                                            stores[entities.decode($(this).html())] = Number($(this).next().text())
                                         }
                                     })
 
                                 
                                 json.stores=(stores)
 
-console.log(json)
+                                collection.insert(json, function (err, doc) {
+        if (err) {
+            // If it failed, return errorprocess.exit()
+            console.log(err)
+            console.log(json)
+            process.exit()
+          // console.log("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+              process.exit()        }
+    });
                             }
 
 
